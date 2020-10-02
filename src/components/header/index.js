b1ec671395bbdabf0001215dcc8297e2a886f051
Ramiro Lima <ramirolima.dev@gmail.com>
2020-10-02 00:26:49 -0300

    refactory header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,18 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React, { useEffect } from "react"
-import style from "./style.module.css"
-import Img from "gatsby-image"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import style from "./style.module.css"
 import clsx from "clsx"
+import Img from "gatsby-image"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
-import { useSelector } from "react-redux"
+import {FaBars, FaTimes} from 'react-icons/fa';
 
 const Header = () => {
+  const [controlMenu, setControlMenu] = useState(false);
+
+  const handleMenu = state => {
+    setControlMenu(state)
+  }
+
   const data = useStaticQuery(graphql`
     query {
       logo: file(relativePath: { eq: "logo.png" }) {
@@ -21,72 +25,28 @@ const Header = () => {
     }
   `)
 
-  const theme = useSelector(state => state.theme.theme)
-
   return (
-    <header
-      style={{ background: "rgba(0,0,0,.3" }}
-      className={style.header}
-    >
-      <div className={style.gridContainer}>
-        <div className={style.logo}>
-          <Img fixed={data.logo.childImageSharp.fixed} draggable={false} />
-        </div>
-        <div
-          style={{ color: theme.textColor.primaryText }}
-          className={clsx(style.itemMenu, style.swing)}
-        >
-          <AnchorLink to="/#anchorAbout">
-            <span>Sobre</span>
-          </AnchorLink>
-        </div>
-        <div
-          style={{ color: theme.textColor.primaryText }}
-          className={clsx(style.itemMenu, style.swing)}
-        >
-          <AnchorLink to="/#anchorSkills">
-            <span>Skills</span>
-          </AnchorLink>
-        </div>
-        <div
-          style={{ color: theme.textColor.primaryText }}
-          className={clsx(style.itemMenu, style.swing)}
-        >
-          <AnchorLink to="/#anchorProjects">
-            <span>Projetos</span>
-          </AnchorLink>
-        </div>
-        <div
-          style={{ color: theme.textColor.primaryText }}
-          className={clsx(style.itemMenu, style.swing)}
-        >
-          <AnchorLink to="/#anchorExperiences">
-            <span>Experiências</span>
-          </AnchorLink>
-        </div>
-        <div
-          style={{ color: theme.textColor.primaryText }}
-          className={clsx(style.itemMenu, style.swing)}
-        >
-          {/* <AnchorLink to="/#anchorBlog"> */}
-            <span>Blog</span>
-          {/* </AnchorLink> */}
-        </div>
+
+    <header>
+      <div className={style.logo}>
+        <Img fixed={data.logo.childImageSharp.fixed} draggable={false} style={{ marginTop: "10px" }} />
+      </div>
+      <nav className={clsx({ [style.active]: controlMenu })}>
+        <ul>
+          <li onClick={() => handleMenu(false)}><AnchorLink to="/#anchorAbout"><span>Sobre</span></AnchorLink></li>
+          <li onClick={() => handleMenu(false)}><AnchorLink to="/#anchorSkills"><span>Skills</span></AnchorLink></li>
+          <li onClick={() => handleMenu(false)}><AnchorLink to="/#anchorProjects"><span>Projetos</span></AnchorLink></li>
+          <li onClick={() => handleMenu(false)}><AnchorLink to="/#anchorExperiences"><span>Experiências</span></AnchorLink></li>
+          <li onClick={() => handleMenu(false)}><AnchorLink to="/#anchorBlog"><span>Blog</span></AnchorLink></li>
+        </ul>
+      </nav>
+      <div className={style.menuToggle} onClick={() => handleMenu(!controlMenu)}>
+        {!controlMenu 
+          ? <FaBars className={style.icon}/> 
+          : <FaTimes className={style.icon}/>}
       </div>
     </header>
   )
 }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-Header.defaultProps = {
-  siteTitle: `Title`,
-}
-
 export default Header
-
-// <Link
-//         to="/"
-//         className={style.link}
-//       ></Link>
